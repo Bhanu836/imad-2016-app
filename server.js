28dbd7c4c455816c4e07755138ba54a176ab1ce7
Bhanu <Bhanugyani@gmail.com>
2016-11-11 17:48:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool =require('pg').pool;
+var pool =require('pg').Pool;
 
 var config = {
     user: 'bhanu836',
     database: 'bhanu836',
     host: 'db.imad.hasura-app.io',
     port:'54320',
-    password: process.env.DB-PASSWORD
-    
-    
-};
+    password: process.env.DB_PASSWORD
+    };
 
 var app = express();
 app.use(morgan('combined'));
@@ -19,7 +17,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool = new pool(config);
+var pool = new Pool(config);
 
 app.get('/user-db', function(req, res){
     pool.query('SELECT * FROM user',function(err,result){
