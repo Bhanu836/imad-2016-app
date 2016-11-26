73ccb630db6549d18de8441f7acb1cc2bdcfc7df
Bhanu <Bhanugyani@gmail.com>
2016-11-26 22:25:30 +0530

    Add files via upload

--- corct1.php
+++ corct1.php
@@ -1,13 +1,5 @@
-<html>
-<head></head>
-<body>
 <?php
-include('newcon1.php');
-$dbconnection = new dbconnection();
+$ psql bhanu836
+pg_query("SELECT * FROM user");
 ?>
-Hi <?php echo htmlspecialchars($_POST['name']); ?>
-Your branch <?php echo $_POST['branch']; ?>
-email <?php echo $_POST['email'];?>
-dob <?php echo $_POST['date'];?>
-</body>
-</html>
+						
