494eedeacc0cb98675f2815003c7107a3f31438b
Bhanu <Bhanugyani@gmail.com>
2016-11-28 05:30:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,8 @@ imgo.addEventListener("click", moveleft);
 function stop(){
     window.clearInterval(interval);
     but.innerHTML="start";
-    but.style.width= 30;
+    but1.style.fontSize= 16;
+    
 }
 
 but1.addEventListener("click" ,stop);
