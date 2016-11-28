805929b9647c370db281886bf33f4073f7967c81
Bhanu <Bhanugyani@gmail.com>
2016-11-28 05:32:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ function stop(){
     window.clearInterval(interval);
     but.innerHTML="start";
     but1.style.fontSize= 16;
-     but1.style.zIndex= 1000;
+     but.style.zIndex= 1000;
 }
 
 but1.addEventListener("click" ,stop);
