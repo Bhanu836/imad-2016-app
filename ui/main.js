d95a08fbb63cf94a47f747111d9e43f6bada1a4d
Bhanu <Bhanugyani@gmail.com>
2016-11-28 06:00:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,20 @@ var but =document.getElementById("hbutton");
 var but2 =document.getElementById("but1");
 var marginleft=0;
 var h=0;
-var interval = setInterval(moveleft,10);
+
 function moveleft(){
     marginleft= marginleft + 2 ;
    
     imgo.style.marginLeft=marginleft + "px" ;
+    var interval = setInterval(moveleft,10);
+    var btn3 = document.createElement("BUTTON");
+    btn3.innerHTML="stop";
+    btn3.style.fontSize ="large";
+     btn3.style.zIndex= 1000;
+    but2.removechild(btn);  
+    but2.appendChild(btn3);
+     btn3.addEventListener("click" ,stop);
+    
 }
 imgo.addEventListener("click", moveleft);
 function stop(){
@@ -18,7 +27,7 @@ function stop(){
     btn.innerHTML="start";
     btn.style.fontSize ="large";
      btn.style.zIndex= 1000;
-      
+    but2.removechild(but);  
     but2.appendChild(btn);
      btn.addEventListener("click" ,moveleft);
 }
