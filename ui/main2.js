b8f1b01f2fbcc26d152dc37cfd0f66cdf21cdbb7
Bhanu <Bhanugyani@gmail.com>
2016-11-29 19:29:59 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -1,1 +1,22 @@
 
+var pool =require('pg').Pool;
+
+var config = {
+    user: 'bhanu836',
+    database: 'bhanu836',
+    host: 'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+    };
+    app.get('/article-db', function(req, res){
+    pool.query('SELECT * FROM user',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+         }
+         else{
+             res.send(JSON.stringify(result));
+             
+         }
+        
+    });
+});
