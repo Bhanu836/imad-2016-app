564a77ff3aff42bdc35c8b79d44fb033a014f3aa
Bhanu <Bhanugyani@gmail.com>
2016-11-29 21:36:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,9 +18,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var pool = new pool(config);
-app.get('/corct1.php', function (req, res) {
-  res.sendFile(path.join(__dirname, 'corct1.php'));
-});
+
 
 app.get('/article-db', function(req, res){
     pool.query('SELECT * FROM user',function(err,result){
