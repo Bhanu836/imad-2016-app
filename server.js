fd7e0a9c0cb8770c332037980213082057325af6
Bhanu <Bhanugyani@gmail.com>
2016-12-27 04:02:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new pool(config);
+
 
 app.get('/article-db', function(req, res){
     
