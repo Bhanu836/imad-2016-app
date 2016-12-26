c3cfbc0ec6d004e60c1eb102d105c345a3789b65
Bhanu <Bhanugyani@gmail.com>
2016-12-27 01:05:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,9 +56,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/nde2.js', function (req, res) {
-  res.sendFile(path.join(__dirname,  'nde2.js'));
-});
+
 
 app.use(bodyParser.urlencoded({
     extended: true
