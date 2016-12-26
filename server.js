54ba3b28d6d6309d13dd994979774eb83a08cd2d
Bhanu <Bhanugyani@gmail.com>
2016-12-27 04:28:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.post('/ui/article-one.html', function(req, res){
       console.log(req.body.user.name);
        console.log(req.body.user.email);
     
-    pool.query('UPDATE bag SET(username,email) = ($req.body.user.name, $req.body.user.name)',function(err, result) {
+    pool.query('UPDATE bag SET(username,email) = (req.body.user.name, req.body.user.name)',function(err, result) {
       if ( err ) {
         console.log(err);
       } else {
