ef860b1e9eb87469358f37e926aec98c1e0f483a
Bhanu <Bhanugyani@gmail.com>
2016-12-29 03:37:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ res.end();
      console.log(req.body.stucollage);
      console.log(req.body.collagewt);
      console.log(req.body.citywt);
-    pool.query('INSERT INTO STU_TEACHER(studentname,studentcity,studentcolge_name,wrstteacher_name,wrstteacher_city,wrstteacher_colge_name) VALUES($1,$2,$3,$4,$5,$6)',[req.body.stuname, req.body.stucity, req.body.stucollage, req.body.namewt ,req.body.citywt, req.body.collagewt]);
+    pool.query('INSERT INTO STU_TEACHER(studentname,studentcity,studentcolge_name,wrstteacher_name,wrstteacher_city,wrstteacher_colge_name) VALUES(stunme,stucty,stucollage,namewt,ctywt,collagewt)');
     
     req.on('end',function(stunme,stucollage,stucity,namewt){
         res.write(200,{"Content-Type":"text/html"});
