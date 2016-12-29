d299e98f277dbafd6d4301c23fc4b527f639921d
Bhanu <Bhanugyani@gmail.com>
2016-12-29 18:03:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,9 +18,10 @@ app.use(morgan('combined'));
 var pool = new pool(config);
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-  pool.query('UPDATE countv SET count = count + 1;');
+  
   
   pool.query('UPDATE countv SET date = VALUES($1)',[new Date().toISOString()]);
+  pool.query('UPDATE countv SET count = count + 1;');
   console.log(new Date());
 });
 
