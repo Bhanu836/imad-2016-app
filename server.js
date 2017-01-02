8264e581c47104b7f08ffbc66e8d46f48e2bb4d2
Bhanu <Bhanugyani@gmail.com>
2017-01-02 22:19:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ res.end();
 }
    
 
-pool.query('INSERT INTO stuteacher(studentname,studentcity,studentcolgename,wrstteachername,wrstteachercity,wrstteachercolgename)VALUES($1,$2,$3,$4,$5,$6)',[req.body.stuname,req.body.stucity,req.body.stucollage,req.body.namewt,req.body.citywt,req.body.collagewt]);
+pool.query('INSERT INTO stubook(studentname,studentcity,studentcolgename,wrstteachername,wrstteachercity,wrstteachercolgename)VALUES($1,$2,$3,$4,$5,$6)',[req.body.stuname,req.body.stucity,req.body.stucollage,req.body.namewt,req.body.citywt,req.body.collagewt]);
 
   req.on('end',function(){
     res.write(200,{"Content-Type":"text/html"});  
